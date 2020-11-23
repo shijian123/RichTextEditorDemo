116a742c39ae6a6cb95dda9175955770c574af60
zhangchaoyang <1597681700@qq.com>
2020-11-23 18:33:57 +0800

    修复异常

---
+++ RichTextEditorDemo/YXShowHTMLController.h
@@ -0,0 +1,16 @@
+//
+//  YXShowHTMLController.h
+//  RichTextEditorDemo
+//
+//  Created by zcy on 2020/11/23.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YXShowHTMLController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RichTextEditorDemo/YXShowHTMLController.m
@@ -0,0 +1,31 @@
+//
+//  YXShowHTMLController.m
+//  RichTextEditorDemo
+//
+//  Created by zcy on 2020/11/23.
+//
+
+#import "YXShowHTMLController.h"
+
+@interface YXShowHTMLController ()
+
+@end
+
+@implementation YXShowHTMLController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
